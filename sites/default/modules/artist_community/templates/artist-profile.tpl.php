17ca1f06ce30b2a927e834beb77f32b9ca3a83ef
Kris Arnold <karnold@imamuseum.org>
2013-05-03 13:06:36 -0400

    initial implementation of solr pages and facets

---
+++ sites/default/modules/artist_community/templates/artist-profile.tpl.php
@@ -0,0 +1,11 @@
+<div class="artist-profile">
+    <a href="<?php print $url; ?>">
+        <?php print $image; ?>
+    </a>
+
+    <div class="username">
+        <a href="<?php print $url; ?>">
+            <?php print $user->name; ?>
+        </a>
+    </div>
+</div>
