4bbdcabad373409cad5e23229d35b769234e6956
Matt Gipson <mgipson@imamuseum.org>
2013-06-18 09:41:31 -0400

    front-end theme

--- sites/default/themes/mnartists/layouts/panels/layout_c/layout_c.tpl.php
+++ sites/default/themes/mnartists/layouts/panels/layout_c/layout_c.tpl.php
@@ -12,7 +12,7 @@
  *   added by Adaptivetheme in the appropriate process function.
  * - $panel_suffix: closing element for the $prefix.
  *
- * @see adaptivetheme_preprocess_one()
+ * @see adaptivetheme_preprocess_two_50()
  * @see adaptivetheme_preprocess_node()
  * @see adaptivetheme_process_node()
  */
@@ -25,11 +25,30 @@ $panel_prefix = isset($panel_prefix) ? $panel_prefix : '';
 $panel_suffix = isset($panel_suffix) ? $panel_suffix : '';
 ?>
 <?php print $panel_prefix; ?>
-<div class="at-panel panel-display one-column clearfix" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
-  <div class="region region-one-main">
+<div class="at-panel panel-display two-50 clearfix" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
+  <?php if ($content['two_50_top']): ?>
+    <div class="region region-two-50-top region-conditional-stack">
+      <div class="region-inner clearfix">
+        <?php print $content['two_50_top']; ?>
+      </div>
+    </div>
+  <?php endif; ?>
+  <div class="region region-two-50-first">
     <div class="region-inner clearfix">
-      <?php print $content['one_main']; ?>
+      <?php print $content['two_50_first']; ?>
     </div>
   </div>
+  <div class="region region-two-50-second">
+    <div class="region-inner clearfix">
+      <?php print $content['two_50_second']; ?>
+    </div>
+  </div>
+  <?php if ($content['two_50_bottom']): ?>
+    <div class="region region-two-50-bottom region-conditional-stack">
+      <div class="region-inner clearfix">
+        <?php print $content['two_50_bottom']; ?>
+      </div>
+    </div>
+  <?php endif; ?>
 </div>
 <?php print $panel_suffix; ?>
