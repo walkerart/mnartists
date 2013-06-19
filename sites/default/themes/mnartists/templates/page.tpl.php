9dcf24838b9e79541727c71b6fa6ae94999a62a7
Matt Gipson <mgipson@imamuseum.org>
2013-06-19 11:33:10 -0400

    front-end theme

--- sites/default/themes/mnartists/templates/page.tpl.php
+++ sites/default/themes/mnartists/templates/page.tpl.php
@@ -122,11 +122,11 @@
           <?php if ($title || $primary_local_tasks || $secondary_local_tasks || $action_links = render($action_links)): ?>
             <header<?php print $content_header_attributes; ?>>
 
-              <?php if ($title): ?>
+              <!--<?php if ($title): ?>
                 <h1 id="page-title">
                   <?php print $title; ?>
                 </h1>
-              <?php endif; ?>
+              <?php endif; ?>-->
 
               <?php if ($primary_local_tasks || $secondary_local_tasks || $action_links): ?>
                 <div id="tasks">
