3bead2608267e338ab46d1038f5b495f2b860b82
Matt Gipson <mgipson@imamuseum.org>
2013-06-20 15:34:04 -0400

    front-end theme

--- sites/default/themes/mnartists/layouts/layout_c/layout-c.tpl.php
+++ sites/default/themes/mnartists/layouts/layout_c/layout-c.tpl.php
@@ -21,5 +21,13 @@
 
   <div class="panel-panel panel-col-last">
     <div class="inside"><?php print $content['right']; ?></div>
+    
+    <div class="panel-panel panel-col-first inset-left">
+    	<div class="inside"><?php print $content['inset_left']; ?></div>
+    </div>
+    <div class="panel-panel panel-col-last inset-right">
+    	<div class="inside"><?php print $content['inset_right']; ?></div>
+    </div>
+    
   </div>
 </div>
