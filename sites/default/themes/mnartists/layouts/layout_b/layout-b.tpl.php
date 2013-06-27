296d66ee406cecfd50427558782136162275abb0
Matt Gipson <mgipson@imamuseum.org>
2013-06-27 09:48:52 -0400

    front-end theme

--- sites/default/themes/mnartists/layouts/layout_b/layout-b.tpl.php
+++ sites/default/themes/mnartists/layouts/layout_b/layout-b.tpl.php
@@ -15,11 +15,11 @@
  */
 ?>
 <div class="panel-display panel-2col clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel panel-col-first">
+  <div class="panel-panel panel-col-first main-content">
     <div class="inside"><?php print $content['left']; ?></div>
   </div>
 
-  <div class="panel-panel panel-col-last">
+  <div class="panel-panel panel-col-last sidebar-right">
     <div class="inside"><?php print $content['right']; ?></div>
   </div>
 </div>
