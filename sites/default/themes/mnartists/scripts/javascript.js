fb9aaa6e5a021c1592ec909ffb3eef60c4c02c83
Anton Swartz <djnecrogami@gmail.com>
2013-07-08 15:05:39 -0400

    jquery bug

--- sites/default/themes/mnartists/scripts/javascript.js
+++ sites/default/themes/mnartists/scripts/javascript.js
@@ -1,5 +1,5 @@
 /* DOCUMENT READY STUFF */
 jQuery(document).ready(function(){
-	var count = $(".field-items").children().length;
-    if (count > 1) $(".field-items").slidesjs();
+	var count = jQuery(".field-items").children().length;
+    if (count > 1) jQuery(".field-items").slidesjs();
 });
