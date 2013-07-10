243c59c6f2db7bc14348a3d59b3aeaa1bf31e86d
Kris Arnold <karnold@imamuseum.org>
2013-07-10 16:22:25 -0400

    Saving JS

--- sites/default/themes/mnartists/scripts/javascript.js
+++ sites/default/themes/mnartists/scripts/javascript.js
@@ -1,28 +1,78 @@
 /* DOCUMENT READY STUFF */
 jQuery(document).ready(function(){
+    jQuery(".field-name-field-media, .view-artist-artworks").fitVids();
+    
+    var slideContainerSelectors = [
+        ".field-name-field-media",
+        ".view-artist-artworks"
+    ];
+    var slideSelectors = [
+        ".field-items",
+        ".view-content"
+    ];
+    var slideItemSelectors = [
+        ".field-item",
+        ".views-row"
+    ];
+
+    for (var i = 0, len = slideContainerSelectors.length; i < len; i++) {
+        var slideContainerSelector = slideContainerSelectors[i];
+        var slideSelector = slideSelectors[i];
+        var slideItemSelector = slideItemSelectors[i];
+
+        var slideContainer = jQuery(slideContainerSelector);
+        if (slideContainer == undefined) {
+            continue;
+        }
+        var slideElem = slideContainer.find(slideSelector);
+        var slideItems = slideElem.find(slideItemSelector);
+        var maxHeight = 0;
+
+        if (slideItems.length > 1) {
+            slideItems.each(function(){
+                var testHeight = jQuery(this).height();
+                maxHeight = testHeight > maxHeight ? testHeight : maxHeight; 
+            });
+            slideElem.slidesjs({
+                width: slideContainer.width(),
+                height: maxHeight
+            }); 
+        }
+    }
+/*
 	jQuery.each(jQuery(".field-name-field-media"), function() {
 		var ele = jQuery(this).find(".field-items");
 		var ch = ele.children(".field-item");
 		var n=0;
+        var maxHeight = 0;
 		jQuery.each(ch, function(){
-			if(this.innerHTML != "")
+			if (this.innerHTML != "") {
 				n += 1;
-		})
+                var testHeight = jQuery(this).height();
+                maxHeight = testHeight > maxHeight ? testHeight : maxHeight;
+            }
+		});
 	    if (n > 1) {
 	    	ele.slidesjs({
-	        	height: 550
+                width: jQuery(".field-name-field-media").width(),
+	        	height: maxHeight
 		    });
 	    }
 	});
-	jQuery.each(jQuery(".view.view-artist-artworks.view-id-artist_artworks"), function() {
+	jQuery(), function() {
 		var ele = jQuery(this).find(".view-content");
 		var ch = ele.children(".views-row");
 		var n=0;
 		jQuery.each(ch, function(){
-			if(this.innerHTML != "")
+			if (this.innerHTML != "")
 				n += 1;
-		})
-	    if (n > 1) ele.slidesjs();
+		});
+	    if (n > 1) { 
+            ele.slidesjs({
+                width: jQuery(".view-artist-artworks").width(),
+                height: 550
+            });
+        }
 	});
-    jQuery(".slidesjs-container").fitVids();
+*/
 });
