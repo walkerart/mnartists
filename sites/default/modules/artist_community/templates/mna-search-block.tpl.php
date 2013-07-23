fbad0d1b020d47db86caacac4205eedffb41e2f7
Kris Arnold <karnold@imamuseum.org>
2013-07-23 10:21:09 -0400

    adding additional menu files

---
+++ sites/default/modules/artist_community/templates/mna-search-block.tpl.php
@@ -0,0 +1,6 @@
+<p class="site-description">
+    Make your own MN Artists homepage by filtering communities and content.
+</p>
+<div class="search-container">
+    <input type="search" name="search" id="search" />
+</div>
---
+++ sites/default/modules/mnartist_profiles/templates/mna-my-stuff-menu.tpl.php
@@ -0,0 +1,4 @@
+<div id="my-stuff-menu">
+    <p class="site-description">Manage your account, organize collections, and discover new favorites.</p> 
+    <?php print $links; ?>
+</div>
