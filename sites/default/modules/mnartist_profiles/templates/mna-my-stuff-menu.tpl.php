ccf13e45a14a5ac7af543faa0a6ebf87c11ad91c
Kris Arnold <karnold@imamuseum.org>
2013-07-24 15:41:50 -0400

    adding additional menu items to my stuff

--- sites/default/modules/mnartist_profiles/templates/mna-my-stuff-menu.tpl.php
+++ sites/default/modules/mnartist_profiles/templates/mna-my-stuff-menu.tpl.php
@@ -1,4 +1,5 @@
 <div id="my-stuff-menu">
     <p class="site-description">Manage your account, organize collections, and discover new favorites.</p> 
     <?php print $links; ?>
+    <?php print $editLinks; ?>
 </div>
