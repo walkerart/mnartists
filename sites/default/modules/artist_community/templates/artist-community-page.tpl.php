37be4d613330cf39387f5edf0d511a089dc5699b
Matt Gipson <mgipson@imamuseum.org>
2013-08-20 16:17:08 -0400

    search results css/template

--- sites/default/modules/artist_community/templates/artist-community-page.tpl.php
+++ sites/default/modules/artist_community/templates/artist-community-page.tpl.php
@@ -1,8 +1,10 @@
 <?php foreach($content as $type => $item): ?>
     <div class="search-results content-<?php print $type; ?>">
         <?php foreach($item as $node): ?>
-        	<h3><?php print $type; ?></h3>
-            <div class="item"><?php print $node; ?></div>
+            <div class="item">
+            	<h3><?php print $type; ?></h3>
+            	<?php print $node; ?>
+            </div>
         <?php endforeach; ?>
     </div>
 <?php endforeach; ?>
