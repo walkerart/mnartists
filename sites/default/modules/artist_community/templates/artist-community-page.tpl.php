01c3c257f9f1ac465c804825eae48ed82edf1cf4
Matt Gipson <mgipson@imamuseum.org>
2013-08-21 14:57:04 -0400

    css

--- sites/default/modules/artist_community/templates/artist-community-page.tpl.php
+++ sites/default/modules/artist_community/templates/artist-community-page.tpl.php
@@ -1,10 +1,18 @@
-<?php foreach($content as $type => $item): ?>
-    <div class="search-results content-<?php print $type; ?>">
-        <?php foreach($item as $node): ?>
-            <div class="item item-<?php print $type; ?>">
-            	<h3><?php print $type; ?></h3>
-            	<?php print $node; ?>
-            </div>
-        <?php endforeach; ?>
-    </div>
-<?php endforeach; ?>
+<div class="panel-2col">
+	<div class="panel-panel panel-col-first main-content">
+		<?php foreach($content as $type => $item): ?>
+		    <div class="search-results content-<?php print $type; ?>">
+		        <?php foreach($item as $node): ?>
+		            <div class="item item-<?php print $type; ?>">
+		            	<h3><?php print $type; ?></h3>
+		            	<?php print $node; ?>
+		            </div>
+		        <?php endforeach; ?>
+		    </div>
+	    <?php endforeach; ?>
+	</div>
+	<div class="panel-panel panel-col-last sidebar-right">
+		<p>test</p>
+	</div>
+</div>
+
