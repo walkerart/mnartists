9f880426ac231a3030ef558ad0455ca1a29d87fe
David D'Amico <dave_damico@yahoo.ca>
2013-10-15 09:59:34 -0400

    gets less specific in template data construction, MORE specific in iteration over certain fields in template

--- sites/default/modules/mnartist_profiles/templates/mna-resume-download.tpl.php
+++ sites/default/modules/mnartist_profiles/templates/mna-resume-download.tpl.php
@@ -14,7 +14,7 @@
                 padding-bottom: 1em;
                 border-bottom: 1px solid #ccc;
             }
-            .education-block {
+            section > div {
                 margin-bottom: .5em;
             }
             .resume-block label {
@@ -29,7 +29,6 @@
 
     <body>
         <section class="resume-block general-statement">
-            <!-- @TODO push this logic further back, delegate decisions about display onto the data's type -->
             <h2>General Statement</h2>
             <?= $resume_data['field_general_statement'][0]['value'] ?>
         </section>
@@ -44,11 +43,18 @@
             <?php
                 foreach ($resume_data['field_education'] as $field) {?>
                     <div class="education-block">
-                        <p><label>school</label><?=$field['field_school']?></p>
-                        <p><label>degree</label><?=$field['field_degree']?></p>
-                        <p><label>year</label><?=$field['field_year']?></p>
-                        <p><label>or dates attended</label><?=$field['field_or_dates_attended']?></p>
-                        <p><label>areas of study</label><?=$field['field_areas_of_study']?></p>
+                        <p><label>school</label><?=$field['field_school'][0]['value']?></p>
+                        <p><label>degree</label><?=$field['field_degree'][0]['value']?></p>
+                        <p><label>year</label><?=$field['field_year'][0]['value']?></p>
+                        <p><label>or dates attended</label><?=$field['field_or_dates_attended'][0]['value']?></p>
+                        <p>
+                            <label>areas of study</label><ul>
+                            <?php
+                                foreach ($field['field_areas_of_study'] as $inner_field) {?>
+                                    <li><?=$inner_field['value']?></li>
+                                <?}
+                            ?>
+                        </ul></p>
                     </div>
                 <?}
             ?>
@@ -59,11 +65,11 @@
             <?php
                 foreach ($resume_data['field_work_experience'] as $field) {?>
                     <div class="experience-block">
-                        <p><label>employer</label><?=$field['field_employer']?></p>
-                        <p><label>position</label><?=$field['field_position']?></p>
-                        <p><label>years</label><?=$field['field_years']?></p>
-                        <p><label>description</label><?=$field['field_description_of_position']?></p>
-                        <p><label>url</label><?=$field['field_url']?></p>
+                        <p><label>employer</label><?=$field['field_employer'][0]['value']?></p>
+                        <p><label>position</label><?=$field['field_position'][0]['value']?></p>
+                        <p><label>years</label><?=$field['field_years'][0]['value']?></p>
+                        <p><label>description</label><?=$field['field_description_of_position'][0]['value']?></p>
+                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
                     </div>
                 <?}
             ?>
@@ -74,12 +80,12 @@
             <?php
                 foreach ($resume_data['field_teaching_experience'] as $field) {?>
                     <div class="experience-block">
-                        <p><label>teaching venue</label><?=$field['field__teaching_venue']?></p>
-                        <p><label>course</label><?=$field['field_course']?></p>
-                        <p><label>year</label><?=$field['field_year']?></p>
-                        <p><label>url</label><?=$field['field_url']?></p>
-                        <p><label>description</label><?=$field['field_description_of_position']?></p>
-                        <p><label>position</label><?=$field['field_position']?></p>
+                        <p><label>teaching venue</label><?=$field['field__teaching_venue'][0]['value']?></p>
+                        <p><label>course</label><?=$field['field_course'][0]['value']?></p>
+                        <p><label>year</label><?=$field['field_year'][0]['value']?></p>
+                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
+                        <p><label>description</label><?=$field['field_description_of_position'][0]['value']?></p>
+                        <p><label>position</label><?=$field['field_position'][0]['value']?></p>
                     </div>
                 <?}
             ?>
@@ -90,10 +96,10 @@
             <?php
                 foreach ($resume_data['field_awards'] as $field) {?>
                     <div class="award-block">
-                        <p><label>award name</label><?=$field['field_award_name']?></p>
-                        <p><label>year</label><?=$field['field_year']?></p>
-                        <p><label>organization</label><?=$field['field_award_organization']?></p>
-                        <p><label>url</label><?=$field['field_url']?></p>
+                        <p><label>award name</label><?=$field['field_award_name'][0]['value']?></p>
+                        <p><label>year</label><?=$field['field_year'][0]['value']?></p>
+                        <p><label>organization</label><?=$field['field_award_organization'][0]['value']?></p>
+                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
                     </div>
                 <?}
             ?>
@@ -104,11 +110,11 @@
             <?php
                 foreach ($resume_data['field_exhibition_and_performance'] as $field) {?>
                     <div class="exhibition-block">
-                        <p><label>title</label><?=$field['field_title']?></p>
-                        <p><label>venue</label><?=$field['field_exhibition_venue']?></p>
-                        <p><label>group/individual</label><?=$field['field_group_individual']?></p>
-                        <p><label>url</label><?=$field['field_url']?></p>
-                        <p><label>venue url</label><?=$field['field_venue_url']?></p>
+                        <p><label>title</label><?=$field['field_title'][0]['value']?></p>
+                        <p><label>venue</label><?=$field['field_exhibition_venue'][0]['value']?></p>
+                        <p><label>group/individual</label><?=$field['field_group_individual'][0]['value']?></p>
+                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
+                        <p><label>venue url</label><?=$field['field_venue_url'][0]['value']?></p>
                     </div>
                 <?}
             ?>
@@ -119,10 +125,10 @@
             <?php
                 foreach ($resume_data['field_publications'] as $field) {?>
                     <div class="publication-block">
-                        <p><label>publication</label><?=$field['field_publication']?></p>
-                        <p><label>title</label><?=$field['field_publication_title']?></p>
-                        <p><label>author/editor</label><?=$field['field_author_editor']?></p>
-                        <p><label>description</label><?=$field['field_description']?></p>
+                        <p><label>publication</label><?=$field['field_publication'][0]['value']?></p>
+                        <p><label>title</label><?=$field['field_publication_title'][0]['value']?></p>
+                        <p><label>author/editor</label><?=$field['field_author_editor'][0]['value']?></p>
+                        <p><label>description</label><?=$field['field_description'][0]['value']?></p>
                     </div>
                 <?}
             ?>
@@ -133,9 +139,9 @@
             <?php
                 foreach ($resume_data['field_galleries'] as $field) {?>
                     <div class="gallery-block">
-                        <p><label>gallery name</label><?=$field['field_gallery_name']?></p>
-                        <p><label>contact name</label><?=$field['field_contact_name']?></p>
-                        <p><label>url</label><?=$field['field_url']?></p>
+                        <p><label>gallery name</label><?=$field['field_gallery_name'][0]['value']?></p>
+                        <p><label>contact name</label><?=$field['field_contact_name'][0]['value']?></p>
+                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
                     </div>
                 <?}
             ?>
@@ -146,9 +152,16 @@
             <?php
                 foreach ($resume_data['field_collections'] as $field) {?>
                     <div class="collection-block">
-                        <p><label>collection name</label><?=$field['field_collection_name']?></p>
-                        <p><label>works</label><?=$field['field_works']?></p><!-- @TODO this (and any other plural value) isn't getting pulled into template_data correctly -->
-                        <p><label>url</label><?=$field['field_url']?></p>
+                        <p><label>collection name</label><?=$field['field_collection_name'][0]['value']?></p>
+                        <p>
+                            <label>works</label><ul>
+                            <?php
+                                foreach ($field['field_works'] as $inner_field) {?>
+                                    <li><?=$inner_field['value']?></li>
+                                <?}
+                            ?>
+                        </ul></p>
+                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
                     </div>
                 <?}
             ?>
