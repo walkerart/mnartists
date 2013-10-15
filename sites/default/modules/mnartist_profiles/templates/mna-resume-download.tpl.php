13f07a9451ed163be875f2386054e61ed1e5e225
David D'Amico <dave_damico@yahoo.ca>
2013-10-15 12:11:38 -0400

    in resume download, bundles field labels with the field values for use in templates, switches to using them in the template

--- sites/default/modules/mnartist_profiles/templates/mna-resume-download.tpl.php
+++ sites/default/modules/mnartist_profiles/templates/mna-resume-download.tpl.php
@@ -28,29 +28,30 @@
     </head>
 
     <body>
-        <section class="resume-block general-statement">
-            <h2>General Statement</h2>
-            <?= $resume_data['field_general_statement'][0]['value'] ?>
+
+        <section class="resume-block resume-block-simple general-statement">
+            <h2><?= $resume_data['field_general_statement']['label'] ?></h2>
+            <?= $resume_data['field_general_statement']['item'][0]['value'] ?>
         </section>
 
-        <section class="resume-block biography">
-            <h2>Biography</h2>
-            <?= $resume_data['field_biography'][0]['value'] ?>
+        <section class="resume-block resume-block-simple biography">
+            <h2><?= $resume_data['field_general_statement']['label'] ?></h2>
+            <?= $resume_data['field_biography']['item'][0]['value'] ?>
         </section>
 
-        <section class="resume-block education">
-            <h2>Education</h2>
+        <section class="resume-block resume-block-complex education">
+            <h2><?= $resume_data['field_education']['label'] ?></h2>
             <?php
-                foreach ($resume_data['field_education'] as $field) {?>
+                foreach ($resume_data['field_education']['item'] as $field) {?>
                     <div class="education-block">
-                        <p><label>school</label><?=$field['field_school'][0]['value']?></p>
-                        <p><label>degree</label><?=$field['field_degree'][0]['value']?></p>
-                        <p><label>year</label><?=$field['field_year'][0]['value']?></p>
-                        <p><label>or dates attended</label><?=$field['field_or_dates_attended'][0]['value']?></p>
+                        <p><label><?= $field['field_school']['label'] ?></label><?=$field['field_school']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_degree']['label']?></label><?=$field['field_degree']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_year']['label']?></label><?=$field['field_year']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_or_dates_attended']['label']?></label><?=$field['field_or_dates_attended']['item'][0]['value']?></p>
                         <p>
-                            <label>areas of study</label><ul>
+                            <label><?=$field['field_areas_of_study']['label']?></label><ul>
                             <?php
-                                foreach ($field['field_areas_of_study'] as $inner_field) {?>
+                                foreach ($field['field_areas_of_study']['item'] as $inner_field) {?>
                                     <li><?=$inner_field['value']?></li>
                                 <?}
                             ?>
@@ -60,130 +61,130 @@
             ?>
         </section>
 
-        <section class="resume-block work-experience">
-            <h2>Work Experience</h2>
+        <section class="resume-block resume-block-complex work-experience">
+            <h2><?= $resume_data['field_work_experience']['label'] ?></h2>
             <?php
-                foreach ($resume_data['field_work_experience'] as $field) {?>
+                foreach ($resume_data['field_work_experience']['item'] as $field) {?>
                     <div class="experience-block">
-                        <p><label>employer</label><?=$field['field_employer'][0]['value']?></p>
-                        <p><label>position</label><?=$field['field_position'][0]['value']?></p>
-                        <p><label>years</label><?=$field['field_years'][0]['value']?></p>
-                        <p><label>description</label><?=$field['field_description_of_position'][0]['value']?></p>
-                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
+                        <p><label><?=$field['field_employer']['label']?></label><?=$field['field_employer']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_position']['label']?></label><?=$field['field_position']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_years']['label']?></label><?=$field['field_years']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_description_of_position']['label']?></label><?=$field['field_description_of_position']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_url']['label']?></label><?=$field['field_url']['item'][0]['value']?></p>
                     </div>
                 <?}
             ?>
         </section>
 
-        <section class="resume-block teaching-experience">
-            <h2>Teaching Experience</h2>
+        <section class="resume-block resume-block-complex teaching-experience">
+            <h2><?= $resume_data['field_teaching_experience']['label'] ?></h2>
             <?php
-                foreach ($resume_data['field_teaching_experience'] as $field) {?>
+                foreach ($resume_data['field_teaching_experience']['item'] as $field) {?>
                     <div class="experience-block">
-                        <p><label>teaching venue</label><?=$field['field__teaching_venue'][0]['value']?></p>
-                        <p><label>course</label><?=$field['field_course'][0]['value']?></p>
-                        <p><label>year</label><?=$field['field_year'][0]['value']?></p>
-                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
-                        <p><label>description</label><?=$field['field_description_of_position'][0]['value']?></p>
-                        <p><label>position</label><?=$field['field_position'][0]['value']?></p>
+                        <p><label><?=$field['field__teaching_venue']['label']?></label><?=$field['field__teaching_venue']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_course']['label']?></label><?=$field['field_course']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_year']['label']?></label><?=$field['field_year']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_url']['label']?></label><?=$field['field_url']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_description_of_position']['label']?></label><?=$field['field_description_of_position']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_position']['label']?></label><?=$field['field_position']['item'][0]['value']?></p>
                     </div>
                 <?}
             ?>
         </section>
 
-        <section class="resume-block awards">
-            <h2>Awards</h2>
+        <section class="resume-block resume-block-complex awards">
+            <h2><?= $resume_data['field_awards']['label'] ?></h2>
             <?php
-                foreach ($resume_data['field_awards'] as $field) {?>
+                foreach ($resume_data['field_awards']['item'] as $field) {?>
                     <div class="award-block">
-                        <p><label>award name</label><?=$field['field_award_name'][0]['value']?></p>
-                        <p><label>year</label><?=$field['field_year'][0]['value']?></p>
-                        <p><label>organization</label><?=$field['field_award_organization'][0]['value']?></p>
-                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
+                        <p><label><?=$field['field_award_name']['label']?></label><?=$field['field_award_name']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_year']['label']?></label><?=$field['field_year']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_award_organization']['label']?></label><?=$field['field_award_organization']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_url']['label']?></label><?=$field['field_url']['item'][0]['value']?></p>
                     </div>
                 <?}
             ?>
         </section>
 
-        <section class="resume-block exhibition-and-performance">
-            <h2>Exhibition and Performance</h2>
+        <section class="resume-block resume-block-complex exhibition-and-performance">
+            <h2><?= $resume_data['field_exhibition_and_performance']['label'] ?></h2>
             <?php
-                foreach ($resume_data['field_exhibition_and_performance'] as $field) {?>
+                foreach ($resume_data['field_exhibition_and_performance']['item'] as $field) {?>
                     <div class="exhibition-block">
-                        <p><label>title</label><?=$field['field_title'][0]['value']?></p>
-                        <p><label>venue</label><?=$field['field_exhibition_venue'][0]['value']?></p>
-                        <p><label>group/individual</label><?=$field['field_group_individual'][0]['value']?></p>
-                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
-                        <p><label>venue url</label><?=$field['field_venue_url'][0]['value']?></p>
+                        <p><label><?=$field['field_title']['label']?></label><?=$field['field_title']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_exhibition_venue']['label']?></label><?=$field['field_exhibition_venue']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_group_individual']['label']?></label><?=$field['field_group_individual']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_url']['label']?></label><?=$field['field_url']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_venue_url']['label']?></label><?=$field['field_venue_url']['item'][0]['value']?></p>
                     </div>
                 <?}
             ?>
         </section>
 
-        <section class="resume-block publications">
-            <h2>Publications</h2>
+        <section class="resume-block resume-block-complex publications">
+            <h2><?= $resume_data['field_publications']['label'] ?></h2>
             <?php
-                foreach ($resume_data['field_publications'] as $field) {?>
+                foreach ($resume_data['field_publications']['item'] as $field) {?>
                     <div class="publication-block">
-                        <p><label>publication</label><?=$field['field_publication'][0]['value']?></p>
-                        <p><label>title</label><?=$field['field_publication_title'][0]['value']?></p>
-                        <p><label>author/editor</label><?=$field['field_author_editor'][0]['value']?></p>
-                        <p><label>description</label><?=$field['field_description'][0]['value']?></p>
+                        <p><label><?=$field['field_publication']['label']?></label><?=$field['field_publication']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_publication_title']['label']?></label><?=$field['field_publication_title']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_author_editor']['label']?></label><?=$field['field_author_editor']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_description']['label']?></label><?=$field['field_description']['item'][0]['value']?></p>
                     </div>
                 <?}
             ?>
         </section>
 
-        <section class="resume-block galleries">
-            <h2>Galleries</h2>
+        <section class="resume-block resume-block-complex galleries">
+            <h2><?= $resume_data['field_galleries']['label'] ?></h2>
             <?php
-                foreach ($resume_data['field_galleries'] as $field) {?>
+                foreach ($resume_data['field_galleries']['item'] as $field) {?>
                     <div class="gallery-block">
-                        <p><label>gallery name</label><?=$field['field_gallery_name'][0]['value']?></p>
-                        <p><label>contact name</label><?=$field['field_contact_name'][0]['value']?></p>
-                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
+                        <p><label><?=$field['field_gallery_name']['label']?></label><?=$field['field_gallery_name']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_contact_name']['label']?></label><?=$field['field_contact_name']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_url']['label']?></label><?=$field['field_url']['item'][0]['value']?></p>
                     </div>
                 <?}
             ?>
         </section>
 
-        <section class="resume-block collections">
-            <h2>Collections</h2>
+        <section class="resume-block resume-block-complex collections">
+            <h2><?= $resume_data['field_collections']['label'] ?></h2>
             <?php
-                foreach ($resume_data['field_collections'] as $field) {?>
+                foreach ($resume_data['field_collections']['item'] as $field) {?>
                     <div class="collection-block">
-                        <p><label>collection name</label><?=$field['field_collection_name'][0]['value']?></p>
+                        <p><label><?=$field['field_collection_name']['label']?></label><?=$field['field_collection_name']['item'][0]['value']?></p>
                         <p>
-                            <label>works</label><ul>
+                            <label><?=$field['field_works']['label'] ?></label><ul>
                             <?php
-                                foreach ($field['field_works'] as $inner_field) {?>
+                                foreach ($field['field_works']['item'] as $inner_field) {?>
                                     <li><?=$inner_field['value']?></li>
                                 <?}
                             ?>
                         </ul></p>
-                        <p><label>url</label><?=$field['field_url'][0]['value']?></p>
+                        <p><label><?=$field['field_url']['label']?></label><?=$field['field_url']['item'][0]['value']?></p>
                     </div>
                 <?}
             ?>
         </section>
 
-        <section class="resume-block related-organizations">
-            <h2>Organizations</h2>
+        <section class="resume-block resume-block-complex related-organizations">
+            <h2><?= $resume_data['field_related_organizations']['label'] ?></h2>
             <?php
-                foreach ($resume_data['field_related_organizations'] as $field) {?>
+                foreach ($resume_data['field_related_organizations']['item'] as $field) {?>
                     <div class="organization-block">
-                        <p><label>organization</label><?=$field['field_organization']?></p>
-                        <p><label>years</label><?=$field['field_years']?></p>
+                        <p><label><?=$field['field_organization']['label']?></label><?=$field['field_organization']['item'][0]['value']?></p>
+                        <p><label><?=$field['field_years']['label']?></label><?=$field['field_years']['item'][0]['value']?></p>
                     </div>
                 <?}
             ?>
         </section>
 
-        <section class="resume-block websites">
-            <h2>Websites</h2>
+        <section class="resume-block resume-block-complex websites">
+            <h2><?= $resume_data['field_websites']['label'] ?></h2>
             <ul>
             <?php
-                foreach ($resume_data['field_websites'] as $site) {?>
+                foreach ($resume_data['field_websites']['item'] as $site) {?>
                     <li><?=$site['title'] ?> - <a href="<?=$site['value'] ?>"><?=$site['value'] ?></a></li>
                 <?}
             ?>
