28866b5b43402d656724dfc424677409c4090c22
David D'Amico <dave_damico@yahoo.ca>
2013-11-14 17:31:05 -0500

    a little extra markup. becuz I said so that’s why!

--- sites/default/modules/mnartist_profiles/templates/mna-feed.tpl.php
+++ sites/default/modules/mnartist_profiles/templates/mna-feed.tpl.php
@@ -3,8 +3,8 @@
     <?php foreach ($feed_data as $feed_event) { ?>
     <li>
         <img src="<?= file_create_url($feed_event->user_picture_uri) ?>" width="50" height="50">
-        <?= $feed_event->message ?>
-        <span class="interval-summary" title="updated time: <?= date('D, d M Y H:i:s', $feed_event->update_timestamp) ?>">
+        <p class="user-feed-message"><?= $feed_event->message ?></p>
+        <span class="user-feed-interval-summary" title="updated time: <?= date('D, d M Y H:i:s', $feed_event->update_timestamp) ?>">
             <?= $feed_event->interval_summary ?>
         </span>
     </li>
