c2d05bf74cdcfc903cebebdde1f00fbb41eb33a8
David D'Amico <dave_damico@yahoo.ca>
2013-12-02 16:23:10 -0500

    get profile pictures at closer-to-correct resolution

--- sites/default/modules/artist_community/templates/artist-profile.tpl.php
+++ sites/default/modules/artist_community/templates/artist-profile.tpl.php
@@ -1,6 +1,10 @@
 <div class="artist-profile">
     <a href="<?php print $url; ?>">
-        <?php print $image; ?>
+        <?php
+            // @TODO better default fallback here
+            $image_uri = (isset($user->picture->uri)) ? image_style_url('square_thumbnail', $user->picture->uri) : 'http://mnartist.imalab.us/sites/default/files/styles/square_thumbnail/public/pictures/picture-default.jpg';
+        ?>
+        <img src="<?= $image_uri ?>" width="100" height="100">
     </a>
 
     <div class="username">
