994cee96c466318e96f13aa3cd5b7280c336a2e5
David D'Amico <dave_damico@yahoo.ca>
2013-12-13 16:58:00 -0500

    pulls in event dispatcher library, replaces the content query with some temp first steps on a manual solr query

--- index.php
+++ index.php
@@ -21,7 +21,11 @@ ini_set('display_startup_errors', TRUE);
 
 
 define('DRUPAL_ROOT', getcwd());
+require_once DRUPAL_ROOT.'/sites/all/libraries/SymfonyEventDispatcher/EventDispatcherInterface.php';
+require_once DRUPAL_ROOT.'/sites/all/libraries/SymfonyEventDispatcher/EventDispatcher.php';
+require_once DRUPAL_ROOT.'/sites/all/libraries/SymfonyEventDispatcher/Event.php';
 require_once DRUPAL_ROOT.'/sites/all/libraries/Solarium/Autoloader.php';
+Solarium\Autoloader::register();
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
