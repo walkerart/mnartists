908d07b019bc77f206f72f882631c072acc31546
David D'Amico <dave_damico@yahoo.ca>
2013-12-13 16:31:14 -0500

    again

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ ini_set('display_startup_errors', TRUE);
 
 
 define('DRUPAL_ROOT', getcwd());
-require_once DRUPAL_ROOT.'/sites/default/libraries/Solarium/Autoloader.php';
+require_once DRUPAL_ROOT.'/sites/all/libraries/Solarium/Autoloader.php';
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
