ce580f0a169563feae47ee73d55cab7969a9020d
David D'Amico <dave_damico@yahoo.ca>
2014-01-07 16:17:38 -0500

    javascript to support the “this is my current position” checkbox business

---
+++ sites/default/modules/mnartist_profiles/js/resume_form.js
@@ -0,0 +1,15 @@
+(function($) {
+    $(document).ready(function() {
+        var targetCheckboxes = $('.field-name-field-work-date-range').find('input[type=checkbox]');
+        targetCheckboxes.on('ifChanged', function (ev) {
+            console.log(this);
+            var endDateFieldsContainer = $(this).parent().parent().parent().parent().find('.field-name-field-work-end-date');
+            if ($(this).is(':checked')) {
+                endDateFieldsContainer.hide();
+            } else {
+                endDateFieldsContainer.show();
+            }
+        });
+        targetCheckboxes.trigger('ifChanged');
+    });
+})(jQuery);
