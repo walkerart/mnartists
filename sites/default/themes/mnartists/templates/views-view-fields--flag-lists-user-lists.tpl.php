23cf5ec41564e0b7cbfa2704861ffb19dd534ad9
David D'Amico <dave_damico@yahoo.ca>
2014-01-14 17:16:08 -0500

    adding wrapper around collection elements

--- sites/default/themes/mnartists/templates/views-view-fields--flag-lists-user-lists.tpl.php
+++ sites/default/themes/mnartists/templates/views-view-fields--flag-lists-user-lists.tpl.php
@@ -12,15 +12,15 @@ $user_list_view->pre_execute();
 $user_list_view->execute();
 
 if (count($user_list_view->result) > 0) { ?>
-    <h2><a href="/<?= $user_list_view->get_url() ?>"><?= $user_list_view->get_title(); ?></a></h2>
-    <?= $user_list_view->render() ?>
+    <div class="user-list-wrap">
+        <h2><a href="/<?= $user_list_view->get_url() ?>"><?= $user_list_view->get_title(); ?></a></h2>
+        <?= $user_list_view->render() ?>
+    </div>
 <?php } else { ?>
-    <h2><?= $user_list_view->get_title(); ?></h2>
-    <div class="view-flag-lists-user-list view-id-flag_lists_user_list view-display-id-page_1">
-        <div class="view-content">
-            <ul class="user-list">
-                <li>Nothing in this collection yet</li>
-            </ul>
-        </div>
+    <div class="user-list-wrap">
+        <h2><?= $user_list_view->get_title(); ?></h2>
+        <ul class="user-list">
+            <li>Nothing in this collection yet</li>
+        </ul>
     </div>
 <?php } ?>
