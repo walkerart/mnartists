dd2b6b6059ad542289a944770d6cc24353d70eea
David D'Amico <dave_damico@yahoo.ca>
2014-01-14 12:44:21 -0500

    adds item-limit to collections in list-of-lists view

--- sites/default/themes/mnartists/templates/views-view-unformatted--flag-lists-user-list.tpl.php
+++ sites/default/themes/mnartists/templates/views-view-unformatted--flag-lists-user-list.tpl.php
@@ -8,12 +8,20 @@ reset($rows);
 $list_title = $view->style_plugin->render_tokens[0]['%1'];
 $list_uri = $view->style_plugin->render_tokens[0]['%q'].'/'.$view->style_plugin->render_tokens[0]['!1'];
 
-// @TODO: consider eliminating single view, just pass id to multiple view to limit
+$row_limit = 9;
+$is_singular_view = (arg(4));// check for list id to figure out if we're int he list-of-lists view or single-list view
+
 ?>
 <h2><a href="/<?= $list_uri ?>"><?= $list_title ?></a></h2>
 <ul class="user-list">
     <?php
         foreach ($rows as $index => $item) {
+            if (!($is_singular_view) && $index === $row_limit) {
+                break;
+            }
             print $item;
     } ?>
 </ul>
+<?php if (!($is_singular_view) && count($rows) > $row_limit) { ?>
+<a class="user-list-more-link" href="/<?= $list_uri ?>">and <?= (count($rows) - $row_limit) ?> more items&hellip;</a>
+<? } ?>
