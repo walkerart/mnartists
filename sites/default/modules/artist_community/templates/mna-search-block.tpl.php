0c47609e1f73a10ebe00929bdb5529e4002403e1
David D'Amico <dave_damico@yahoo.ca>
2014-01-23 15:46:10 -0500

    work on master search

--- sites/default/modules/artist_community/templates/mna-search-block.tpl.php
+++ sites/default/modules/artist_community/templates/mna-search-block.tpl.php
@@ -2,5 +2,18 @@
     Make your own MN Artists homepage by filtering communities and content.
 </p>
 <div class="search-container">
-    <input placeholder="Search" type="search" name="search" id="search" />
+    <form method="get" action="/community">
+        <?php
+            $url_query_string = $_SERVER['QUERY_STRING'];
+            $url_params_work = explode('&', $url_query_string);
+            $url_params = array();
+            foreach ($url_params_work as $item) {
+                $pair = explode('=', $item);
+            ?>
+                <input type="hidden" name="<?= urldecode($pair[0]) ?>" value="<?= urldecode($pair[1]) ?>">
+            <?php }
+        ?>
+
+        <input placeholder="Search" type="search" name="global_search" id="search" />
+    </form>
 </div>
