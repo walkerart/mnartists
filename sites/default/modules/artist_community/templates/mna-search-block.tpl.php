5ccdfe548510b8488348a7409d5c389ffd6f8715
David D'Amico <dave_damico@yahoo.ca>
2014-01-23 16:41:45 -0500

    fix for empty query string issue

--- sites/default/modules/artist_community/templates/mna-search-block.tpl.php
+++ sites/default/modules/artist_community/templates/mna-search-block.tpl.php
@@ -4,16 +4,19 @@
 <div class="search-container">
     <form method="get" action="/community">
         <?php
+            // @TODO probably should filter this down to just OUR params, but this is good for now
             $url_query_string = $_SERVER['QUERY_STRING'];
-            $url_params_work = explode('&', $url_query_string);
-            $url_params = array();
-            foreach ($url_params_work as $item) {
-                $pair = explode('=', $item);
-            ?>
-                <input type="hidden" name="<?= urldecode($pair[0]) ?>" value="<?= urldecode($pair[1]) ?>">
-            <?php }
-        ?>
-
+            if ($url_query_string !== '') {
+                $url_params_work = explode('&', $url_query_string);
+                if (!empty($url_params_work)) {
+                    $url_params = array();
+                    foreach ($url_params_work as $item) {
+                        $pair = explode('=', $item);
+                    ?>
+                        <input type="hidden" name="<?= urldecode($pair[0]) ?>" value="<?= urldecode($pair[1]) ?>">
+                    <?php }
+                }
+            } ?>
         <input placeholder="Search" type="search" name="global_search" id="search" />
     </form>
 </div>
