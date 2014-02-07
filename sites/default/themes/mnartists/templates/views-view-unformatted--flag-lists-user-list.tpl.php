c5cd3276ab7ae66b847a032692257ce9a90447fd
David D'Amico <dave_damico@yahoo.ca>
2014-02-07 15:10:22 -0500

    changing collection blocks so that they always use the /user/uid form of the link to their singular versions

--- sites/default/themes/mnartists/templates/views-view-unformatted--flag-lists-user-list.tpl.php
+++ sites/default/themes/mnartists/templates/views-view-unformatted--flag-lists-user-list.tpl.php
@@ -4,10 +4,14 @@
 // (see views-view-fields--flag-lists-user-list.tpl.php) for
 // the rendering of the individual items in the list
 
+global $user;
+
 reset($rows);
 
 $row_limit = 9;
-$is_singular_view = (is_numeric(arg(2)));// check for list id to figure out if we're in the list-of-lists view or single-list view
+// check for list id to figure out if we're in the list-of-lists view or single-list view
+$is_singular_view = ((arg(0) === 'user' && is_numeric(arg(4))) || (arg(0) === 'flags' && is_numeric(arg(2))));
+$link_url = 'user/'.$user->uid.'/'.$view->get_url();
 
 ?>
 <?php if ($is_singular_view) { ?>
@@ -23,5 +27,5 @@ $is_singular_view = (is_numeric(arg(2)));// check for list id to figure out if w
     } ?>
 </ul>
 <?php if (!($is_singular_view) && count($rows) > $row_limit) { ?>
-<a class="user-list-more-link" href="/<?= $view->get_url() ?>">and <?= (count($rows) - $row_limit) ?> more items&hellip;</a>
+<a class="user-list-more-link" href="/<?= $link_url ?>">and <?= (count($rows) - $row_limit) ?> more items&hellip;</a>
 <?php } ?>
