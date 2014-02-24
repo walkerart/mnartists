6c58baa3c16fe4a7b08fb1addcc93dfdeef2afe5
David D'Amico <dave_damico@yahoo.ca>
2014-02-24 14:29:54 -0500

    extra classes in star menu

--- sites/default/modules/mnartist_collections/templates/node_star.tpl.php
+++ sites/default/modules/mnartist_collections/templates/node_star.tpl.php
@@ -9,7 +9,7 @@
         <ul>
             <li class='collect-this'><?= flag_create_link('collections', $node_id) ?></li>
             <hr>
-            <li class='add-this'>ADD TO:</li>
+            <li class='add-this collect-title'>ADD TO:</li>
             <?php if(is_array($flags)) {
                 foreach($flags as $row) { ?>
                     <li class='flag-this'><?= $row ?></li>
@@ -17,7 +17,7 @@
             } ?>
             <li class='add-this last'><?= l(t('START A NEW @name', array('@name' => strtoupper(variable_get('flag_lists_name', t('list'))))), 'flag-lists/add/' . $node->type, array('query' => _mnartist_collections_fix_community_destination(drupal_get_destination()))) ?></li>
             <hr>
-            <li class='share-this'>SHARE VIA:</li>
+            <li class='share-this collect-title'>SHARE VIA:</li>
             <li class='share-this'><a href='#' onclick='window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent("<?= $node_url ?>"), "facebook-share-dialog", "width=626,height=436"); return false;'>FACEBOOK</a></li>
             <li class='share-this'><a href='#' onclick='window.open("https://twitter.com/share?url=" + encodeURIComponent("<?= $node_url ?>"), "twitter-share-dialog", "width=626,height=436"); return false;'>TWITTER</a></li>
             <li><a href='#'>ADD A NETWORK&hellip;</a></li>
