f54da7320d9172c88c92cac63f651b665918208e
Matt Gipson <mgipson@imamuseum.org>
2014-03-19 12:43:28 -0400

    made some template changes to layout-a testing at this point

--- sites/default/themes/mnartists/layouts/layout_a/layout-a.tpl.php
+++ sites/default/themes/mnartists/layouts/layout_a/layout-a.tpl.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Template for a 2 column panel layout.
+ * Template for a 2 column panel layout. this is for community page(s)
  *
  * This template provides a two column panel display layout, with
  * each column roughly equal in width.
@@ -15,9 +15,17 @@
  */
 ?>
 <div class="panel-display panel-2col clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-pane panel-col-top">
+    <div class="panel-col-first">
+      <div class="inside"><?php print $content['top_left']; ?></div>
+    </div>
+    <div class="panel-col-last">
+      <div class="inside"><?php print $content['top_right']; ?></div>
+    </div>
+  </div>
   <div class="panel-panel panel-col-first main-content">
     <div class="inside"><?php print $content['left']; ?></div>
-    
+
     <div class="panel-panel panel-col-first inset-left">
     	<div class="inside"><?php print $content['inset_left']; ?></div>
     </div>
