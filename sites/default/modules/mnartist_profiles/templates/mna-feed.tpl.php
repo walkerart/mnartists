c060f017ed5eef5597cb685e2dbe3f475b3c5aae
David D'Amico <dave_damico@yahoo.ca>
2014-03-26 16:49:25 -0400

    adds link around user photo in feed items

--- sites/default/modules/mnartist_profiles/templates/mna-feed.tpl.php
+++ sites/default/modules/mnartist_profiles/templates/mna-feed.tpl.php
@@ -4,7 +4,7 @@
         if (count($feed_data) > 0) {
             foreach ($feed_data as $feed_event) { ?>
                 <li class="feed-item">
-                    <img src="<?= file_create_url($feed_event->user_picture_uri) ?>" width="50" height="50">
+                    <a href="<?= $feed_event->entity_id ?>"><img src="<?= file_create_url($feed_event->user_picture_uri) ?>" width="50" height="50"></a>
                     <div class="user-feed-message-container">
                         <?= $feed_event->message ?>
                         <div class="user-feed-message-star"><?= theme("mnartist_collections_star", array('node_id' => $feed_event->entity_id)) ?></div>
