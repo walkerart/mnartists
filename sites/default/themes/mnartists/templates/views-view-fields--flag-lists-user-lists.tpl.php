feb2f814d6a3ecc7a603a1f1b46aab2f7d4cd643
David D'Amico <dave_damico@yahoo.ca>
2014-04-09 15:25:17 -0400

    had the edit/delete links backwards, jeez dave get with it

--- sites/default/themes/mnartists/templates/views-view-fields--flag-lists-user-lists.tpl.php
+++ sites/default/themes/mnartists/templates/views-view-fields--flag-lists-user-lists.tpl.php
@@ -14,11 +14,19 @@ $user_list_view->execute();
 if (count($user_list_view->result) > 0) { ?>
     <div class="user-list-wrap">
         <h2><a href="/user/<?= $user->uid ?>/<?= $user_list_view->get_url() ?>"><?= $user_list_view->get_title(); ?></a></h2>
+        <ul class="edit-controls">
+            <li><a href="/flags/lists/edit/<?= $list_id ?>">edit</a></li>
+            <li><a href="/flags/lists/delete/<?= $list_id ?>">delete</a></li>
+        </ul>
         <?= $user_list_view->render() ?>
     </div>
 <?php } else { ?>
     <div class="user-list-wrap">
         <h2><?= $user_list_view->get_title(); ?></h2>
+        <ul class="edit-controls">
+            <li><a href="/flags/lists/edit/<?= $list_id ?>">edit</a></li>
+            <li><a href="/flags/lists/delete/<?= $list_id ?>">delete</a></li>
+        </ul>
         <ul class="user-list">
             <li>Nothing in this collection yet</li>
         </ul>
