bade0ad0a017d6efb99f4f3f71ec8395fae49700
David D'Amico <dave_damico@yahoo.ca>
2014-04-10 18:27:25 -0400

    moves edit controls into singular collection template

--- sites/default/themes/mnartists/templates/views-view-unformatted--flag-lists-user-list.tpl.php
+++ sites/default/themes/mnartists/templates/views-view-unformatted--flag-lists-user-list.tpl.php
@@ -12,11 +12,16 @@ $row_limit = 9;
 // check for list id to figure out if we're in the list-of-lists view or single-list view
 $is_singular_view = ((arg(0) === 'user' && is_numeric(arg(4))) || (arg(0) === 'flags' && is_numeric(arg(2))));
 $link_url = 'user/'.$user->uid.'/'.$view->get_url();
+$list_id = $view->args[0];
 
 ?>
 <?php if ($is_singular_view) { ?>
 <h2><?= $view->get_title() ?></h2>
 <?php } ?>
+<ul class="edit-controls">
+    <li class="edit"><a href="/flags/lists/edit/<?= $list_id ?>">edit</a></li>
+    <li class="delete"><a href="/flags/lists/delete/<?= $list_id ?>">delete</a></li>
+</ul>
 <ul class="user-list">
     <?php
         foreach ($rows as $index => $item) {
