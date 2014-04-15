34f024f45bd452e4c83b1bd0807c4aec33cb9eb5
David D'Amico <dave_damico@yahoo.ca>
2014-04-15 11:20:41 -0400

    article sidebar work

--- sites/default/themes/mnartists/templates/panels-pane--node-author--node-author.tpl.php
+++ sites/default/themes/mnartists/templates/panels-pane--node-author--node-author.tpl.php
@@ -1,11 +1,18 @@
 <?php
 	$article_node = node_load($display->args[0]);
 	$article_author = user_load($article_node->uid);
+    $author_uri = "/users/$article_author->uid";
 	$full_name = mnartist_profiles_collective_or_fullname_or_username($article_author->uid);
 	$author_image_uri = image_style_url('square_thumbnail', $article_author->picture->uri);
 ?>
-<?php if ($title) { ?>
-  <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
-<?php } ?>
-<img src="<?= $author_image_uri ?>" alt="$full_name">
-<?= $full_name ?>
+<div<?php print $attributes; ?>>
+    <?php if ($title) { ?>
+      <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+    <?php } ?>
+    <div class="widget-content">
+        <a href="<?= $author_uri ?>">
+            <img src="<?= $author_image_uri ?>" alt="<?= $full_name ?>">
+            <span class="author-full-name"><?= $full_name ?></span>
+        </a>
+    </div>
+</div>
