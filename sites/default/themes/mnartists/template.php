1b0d0c3c7a7d6088e2a49bbab2a9421bae253e10
David D'Amico <dave_damico@yahoo.ca>
2014-04-23 17:22:32 -0400

    override for mailchimp manage subscriptions tab menu type, switches the mailchimp page over to my-stuff

--- sites/default/themes/mnartists/template.php
+++ sites/default/themes/mnartists/template.php
@@ -37,7 +37,7 @@ function mnartists_preprocess_html(&$vars) {
   $vars['classes_array'][] = css_browser_selector();
 
   if ((!user_is_logged_in() && arg(0) == "user" && (is_null(arg(1)) || in_array(arg(1), array('password', 'register')))) ||
-          (arg(0) == "user" && in_array(arg(2), array("edit", "flags"))) ||
+          (arg(0) == "user" && in_array(arg(2), array("edit", "flags", "mailchimp"))) ||
           (arg(0) == "node" && in_array(arg(1), array("edit", "add"))) ||
           (arg(0) == "node" && in_array(arg(2), array("edit"))) ||
           (arg(0) == 'user' && in_array(arg(1), array("saved-filters", "feed")))
