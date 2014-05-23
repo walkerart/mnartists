1c9e24230cccc2b09daa60b0840ea417c5d6f965
David D'Amico <dave_damico@yahoo.ca>
2014-05-23 14:22:26 -0400

    fixing some bugginess in welcome page showing/not-showing

--- sites/default/modules/mnartist_profiles/templates/mna-user-welcome.tpl.php
+++ sites/default/modules/mnartist_profiles/templates/mna-user-welcome.tpl.php
@@ -1,3 +1,7 @@
+<?php
+    global $user;
+    $should_show_welcome = _mnartist_profiles_should_show_welcome();
+?>
 <div class="user-welcome">
     <h2>Now that you've joined MN Artists, here are some fun ways to start exploring and better using the site.</h2>
     <ul class="welcome-list">
@@ -9,6 +13,6 @@
         <li class="welcome-events">Learn about what art events are happening this week</li>
     </ul>
     <form id="welcome-form" class="done-link" method="post" action="/user/<?= $user->uid ?>/setwelcome">
-        <label><input type="checkbox" name="no-welcome"<?php if (isset($ive_tried_this) && $ive_tried_this === 1) { ?> checked<?php } ?>> I've tried all these things</label>
+        <label><input type="checkbox" name="no-welcome"<?php if (!$should_show_welcome) { ?> checked<?php } ?>> I've tried all these things</label>
     </form>
 </div>
