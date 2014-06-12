0c98e147b73090d05479768195a1cc68aa56ac9e
Matt Gipson <mgipson@imamuseum.org>
2014-06-12 11:53:24 -0400

    css

--- sites/default/modules/mnartist_profiles/templates/mna-user-welcome.tpl.php
+++ sites/default/modules/mnartist_profiles/templates/mna-user-welcome.tpl.php
@@ -6,8 +6,8 @@
     <h2>Now that you've joined MN Artists, here are some fun ways to start exploring and better using the site.</h2>
     <ul class="welcome-list">
         <li class="welcome-artists"><a href="/community?content[artists]=1">Find new artists in your area</a></li>
-        <li class="welcome-collect"><a href="/">Start collecting artists, artworks, or articles</li>
-        <li class="welcome-articles"><a href="/community?content[article]=1">Read up on current arts articles</li>
+        <li class="welcome-collect"><a href="/">Start collecting artists, artworks, or articles</a></li>
+        <li class="welcome-articles"><a href="/community?content[article]=1">Read up on current arts articles</a></li>
         <li class="welcome-artwork"><a href="/node/add/artwork">Add new work to your profile</a></li>
         <li class="welcome-opportunities"><a href="/community?content[opportunity]=1">Find Opportunities to advance your career</a></li>
         <li class="welcome-events"><a href="/community?content[event]=1">Learn about what art events are happening this week</a></li>
