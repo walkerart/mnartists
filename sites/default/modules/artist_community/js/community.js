16255336df542b485736faf4a4ac8940641c706a
David D'Amico <dave_damico@yahoo.ca>
2014-06-19 12:08:15 -0400

    changes new star menu markup to look more like pre-existing menu markup, updates javascript to deal with that menu stuff, makes base_url global available in the artist profile template

--- sites/default/modules/artist_community/js/community.js
+++ sites/default/modules/artist_community/js/community.js
@@ -117,5 +117,39 @@
         // never be able to get low enough in the viewport to
         // trigger the disappearing sorter
 
+        // init the follow-menu items
+        $('.follow-menu').each(function () {
+            var el = $(this);
+            var starEl = el.find('a.flag-link-toggle');
+            var verbEl = el.find('li.follow-this .flag-verb');
+            var followLinkEl = verbEl.parent();
+
+            if (starEl.hasClass('unflag-action')) {
+                verbEl.text('Unfollow');
+            } else if (starEl.hasClass('flag-action')) {
+                verbEl.text('Follow');
+            }
+
+            followLinkEl.click(function (evt) {
+                evt.preventDefault();
+                var localStarEl = $(evt.target).parent().parent().parent().parent().find('.flag-wrapper a');
+                localStarEl.trigger('click');
+            });
+        });
+
+        // and when a follow/flag action occurs, deal with that
+        $(document).bind('flagGlobalAfterLinkUpdate', function(event, data) {
+            var flagEl = $(data.link);
+            var verbEl = flagEl.siblings('ul').find('li.follow-this a .flag-verb');
+
+            if (verbEl) {
+                if (data.flagStatus === 'unflagged') {
+                    verbEl.text('Follow');
+                } else if (data.flagStatus === 'flagged') {
+                    verbEl.text('Unfollow');
+                }
+            }
+        });
+
     });
 })(jQuery);
