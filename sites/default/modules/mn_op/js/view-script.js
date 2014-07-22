ffd443d478f48960aac67646f8816d37ac9923af
Jeremy Tubbs <jeremytubbs@gmail.com>
2014-07-22 14:30:21 -0400

    Vimeo Thumb and if statement of applications view

---
+++ sites/default/modules/mn_op/js/view-script.js
@@ -0,0 +1,20 @@
+(function($){
+$(document).ready(function(){
+
+	$('.ap-more').click(function() {
+        $(this).prev('.ap-artwork-body').css({'height': 'auto'});
+        $(this).addClass('hidden');
+    });
+
+    $('.ap-artwork-body').click(function() {
+        $(this).css({'height': '96px'});
+        $(this).next('.ap-more').removeClass('hidden');
+    });
+
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+		event.preventDefault();
+		return $(this).ekkoLightbox();
+	});
+
+});
+})(jQuery);
