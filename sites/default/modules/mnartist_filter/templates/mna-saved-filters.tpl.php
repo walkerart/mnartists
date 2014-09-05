1c5ede0690c1f741906eb536626cdda927c6e9e9
Jeremy Tubbs <jeremytubbs@gmail.com>
2014-09-05 14:26:21 -0400

    filter copy

--- sites/default/modules/mnartist_filter/templates/mna-saved-filters.tpl.php
+++ sites/default/modules/mnartist_filter/templates/mna-saved-filters.tpl.php
@@ -1,37 +1,42 @@
-<table>
-    <thead>
-        <tr>
-            <td>Name</td>
-            <td>Filters</td>
-            <td>Options</td>
-        </tr>
-    </thead>
+<h2>MY FILTERS</h2>
+<p class="intro-text">Create new Saved Filters to bookmark and store your searches on MN Artists to create personalized homepages of the content that most interests you.</p>
 
-    <tbody>
-
-        <?php foreach ($filters as $filter) { ?>
+<div class="user-feed">
+    <table>
+        <thead>
             <tr>
-                <td>
-                    <div class="rename-filter-static-state">
-                        <a class="rename-filter-static-title" href="<?= urldecode($filter->url) ?>"><?= $filter->title ?></a>
-                    </div>
-                    <div class="rename-filter-editing-state" style="display: none">
-                        <form action="/user/saved-filters/rename" method="post">
-                            <input type="hidden" name="fid" value="<?= $filter->fid ?>">
-                            <input type="text" name="newtitle" value="<?= $filter->title ?>">
-                            <button type="submit">Submit</button>
-                            <button type="button" class="rename-filter-cancel-button">Cancel</button>
-                        </form>
-                    </div>
-                </td>
-                <td>
-                    <a href="<?= urldecode($filter->url) ?>"><?= $filter->communities_fragment ?></a>
-                </td>
-                <td>
-                    <a href="#" class="filter-rename-button">Rename</a>
-                    <a href="/user/saved-filters/delete?fid=<?= $filter->fid ?>">Delete</a>
-                </td>
-        <?php } ?>
+                <td>Name</td>
+                <td>Filters</td>
+                <td>Options</td>
+            </tr>
+        </thead>
 
-    </tbody>
-</table>
+        <tbody>
+
+            <?php foreach ($filters as $filter) : ?>
+                <tr>
+                    <td>
+                        <div class="rename-filter-static-state">
+                            <a class="rename-filter-static-title" href="<?= urldecode($filter->url) ?>"><?= $filter->title ?></a>
+                        </div>
+                        <div class="rename-filter-editing-state" style="display: none">
+                            <form action="/user/saved-filters/rename" method="post">
+                                <input type="hidden" name="fid" value="<?= $filter->fid ?>">
+                                <input type="text" name="newtitle" value="<?= $filter->title ?>">
+                                <button type="submit">Submit</button>
+                                <button type="button" class="rename-filter-cancel-button">Cancel</button>
+                            </form>
+                        </div>
+                    </td>
+                    <td>
+                        <a href="<?= urldecode($filter->url) ?>"><?= $filter->communities_fragment ?></a>
+                    </td>
+                    <td>
+                        <a href="#" class="filter-rename-button">Rename</a>
+                        <a href="/user/saved-filters/delete?fid=<?= $filter->fid ?>">Delete</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
