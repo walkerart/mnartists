2679060f2af12f1e6595562c8decd93923690256
Jeremy Tubbs <jeremytubbs@gmail.com>
2014-09-08 14:10:02 -0400

    welcome register message

--- sites/default/modules/mnartist_profiles/templates/mna-my-stuff-menu.tpl.php
+++ sites/default/modules/mnartist_profiles/templates/mna-my-stuff-menu.tpl.php
@@ -3,7 +3,20 @@
     global $user;
 ?>
 <div id="my-stuff-menu">
-    <p class="site-description">Hi <?php print mnartist_profiles_collective_or_fullname_or_username($user->uid); ?>, use the left-hand menu to manage your account, profile, artwork, events, collections, resume and applications.</p>
-    <?php print $links; ?>
-    <?php print $editLinks; ?>
+    <p class="site-description">
+    <?php if($user->uid) : ?>
+    	Hi <?php print mnartist_profiles_collective_or_fullname_or_username($user->uid); ?>, use the left-hand menu to manage your account, profile, artwork, events, collections, resume and applications.
+    <?php else : ?>
+    	Welcome to the new MN Artists! To move your artwork and information from the old site to the new site, <a class="my-stuff-link" href="<?php echo url('user/register', array()); ?>">click here</a>.
+    <?php endif; ?>
+    </p>
+    <?php if(isset($loginItems)) : ?>
+    	<ul>
+    	<?php foreach ($loginItems as $item) : ?>
+    		<li><?php print $item; ?></li>
+    	<?php endforeach; ?>
+    	</ul>
+    <?php endif; ?>
+    <?php if(isset($renderedLinks)) print $renderedLinks; ?>
+    <?php if(isset($editLinks)) print $editLinks; ?>
 </div>
