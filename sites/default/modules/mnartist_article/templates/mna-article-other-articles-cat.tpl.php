292d8a5ebfdf8628af4c6f1e7d82216d587bbb47
Chin Kiong, Ng <ck@origineight.net>
2014-11-01 08:01:09 -0400

    - simplify and improved performance of the code - fixed existing search query loading all articles to only display 3 (long loading time when there are now 2.5K+ articles) - fixed block's node link to use node path alias instead of node/NID - added loading by byline instead of author when author is MN Artists - fixed the exact coding problems in "More In Performing Arts" block

--- sites/default/modules/mnartist_article/templates/mna-article-other-articles-cat.tpl.php
+++ sites/default/modules/mnartist_article/templates/mna-article-other-articles-cat.tpl.php
@@ -1,21 +1,19 @@
 <?php
     // template for rendering the 'other articles in this category'
     // block in the sidebar on the single article view
-if (count($other_articles) > 0) {
 ?>
 <div class="widget">
     <h2>More In <?= $community_title ?></h2>
     <div class="widget-content">
         <ul>
             <?php foreach ($other_articles as $key => $article) { ?>
-                <li><a href="<?= $article->node_uri ?>">
-                    <?php if ($key === 0 && isset($article->image_uri)) { ?>
-                        <img src="<?= $article->image_uri ?>" alt="<?= $article->title ?>">
+                <li><a href="<?= $article['node_uri']; ?>">
+                    <?php if ($key === 0 && isset($article['image_uri'])) { ?>
+                        <img src="<?= $article['image_uri']; ?>" alt="<?= $article['title']; ?>">
                     <?php } ?>
-                    <span class="other-article-title"><?= $article->title ?></span>
+                    <span class="other-article-title"><?= $article['title']; ?></span>
                 </a></li>
             <?php } ?>
         </ul>
     </div>
 </div>
-<?php } ?>
